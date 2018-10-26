787aea296efabc58b39556fe57670ed55df9766d
tomover9000 <vlad.zorlescu@gmail.com>
2018-10-26 23:23:05 +0300

    All done and checked with some comments

--- main.c
+++ main.c
@@ -22,7 +22,6 @@ void ShowMap(unsigned long long Map) {
 		mask=mask>>1;
 	}
 	printf("\n");
-//	sleep(1);
 }
 
 unsigned long long movedownPiece(unsigned long long Map, int current, int depth) {
@@ -50,15 +49,10 @@ int shiftPiece(unsigned long long Map, int current, int move, int depth) {
 		boundries = 0b0000000100000001000000010000000100000001000000010000000100000001;
 
 		while(move!=0 && (boundries&current_long)==0 && ((current_long>>1)&Map)==0) {
-/*			printf("boundriessss:\n"); 
 
-			ShowMap(boundries&current_long);
-*/
-	
 			move--;
 			current = current>>1;
 			current_long = current_long>>1;
-//			ShowMap(current_long);
 	
 		}
 	}else {
@@ -66,18 +60,11 @@ int shiftPiece(unsigned long long Map, int current, int move, int depth) {
 		boundries = 0b1000000010000000100000001000000010000000100000001000000010000000;
 
 		while(move!=0 && (boundries&current_long)==0 && ((current_long<<1)&Map)==0) {
-/*			
-			printf("boundriessss:\n"); 
 
-			ShowMap(boundries&current_long);
-*/
 			move++;
 			current = current<<1;
 			current_long = current_long<<1;
-			/*printf("mask&curret =%d\n", mask&current);
-			printf("Moving piece \n"); 
-			ShowMap(current_long);
-*/	
+
 		}
 	}
 	return current;
@@ -163,19 +150,22 @@ int main() {
 	ShowMap(Map);
 
 	for(i=0; i<Nr_pieces; i++) {
-//		printf("Piece no %d \n\n", i);
+
 		for(j=1; j<9; j++) { 
 
 			lastPiece = Pieces[i][0];
-		      	Map = movedownPiece(Stable_Map, Pieces[i][0], j-1);
+			//movedown returns 0 if theres a collision
+		    Map = movedownPiece(Stable_Map, Pieces[i][0], j-1);
 			if(Map == 0) {
+				//show map if there's a part of a piece entering the Map
 				if((j == 1) || (j == 2 && Pieces[i][0] > 0b0000000011111111)) {
 					ShowMap(Prev_Map);
 					Stable_Map = Prev_Map;
 				}
 				break;
 			}
-			
+		
+			//shift Piece
 			Pieces[i][0] = shiftPiece(Stable_Map, Pieces[i][0], Pieces[i][j], j-1);
 			Map = addPiece(Stable_Map, Pieces[i][0], j-1);
 			ShowMap(Map);
@@ -184,6 +174,7 @@ int main() {
 			Prev_Map = Map;
 		
 		}
+		//Break the loop if the piece didn't make it full onto the screen
 		if(j == 1 || (j == 2 && Pieces[i][0] > 0b0000000011111111)) {
 			break;
 		}
@@ -195,8 +186,6 @@ int main() {
 		}else {
 			Stable_Map = Prev_Map;
 		}
-			
-		
 	}
 
 	score = sqrt(numberOfZeros(Stable_Map)) + pow(1.25, completedLines);
